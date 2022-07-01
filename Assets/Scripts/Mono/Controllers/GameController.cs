471af91edfb38577f81dd90abb3306d5e8910b8e
Sinekrovka <77225026+Sinekrovka@users.noreply.github.com>
2022-07-01 22:11:00 +0600

    Добавлен экран выигрыша

--- Assets/Scripts/Mono/Controllers/GameController.cs
+++ Assets/Scripts/Mono/Controllers/GameController.cs
@@ -6,21 +6,30 @@ public class GameController : MonoBehaviour
 {
     public static GameController Instance;
     private int timeGame;
+    private GameObject _failScreen;
+    private GameObject _winScreen;
 
     private void Awake()
     {
         Instance = this;
+        _failScreen = GameObject.Find("FailScreen");
+        _winScreen = GameObject.Find("WinScreen");
+        _failScreen.SetActive(false);
+        _winScreen.SetActive(false);
         StartCoroutine(Timer());
     }
 
     private void WinGame()
     {
-        
+        _winScreen.SetActive(true);
+        Time.timeScale = 0f;
     }
 
     public void FailGame()
     {
         StopAllCoroutines();
+        _failScreen.SetActive(true);
+        Time.timeScale = 0f;
     }
 
     private IEnumerator Timer()
